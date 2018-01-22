b62f0958e8ac0c7f2676019201012d1d3d796b78
knknkn1162 <knknkn1162@gmail.com>
2018-01-22 16:17:53 +0900

    view test

--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/home.html.erb", type: :view do
+  it 'displays title' do
+    render template: 'static_pages/home', layout: 'layouts/application'
+    expect(rendered).to have_title full_title('Home')
+  end
 end
