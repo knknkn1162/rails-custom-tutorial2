cc2fd2f5eed29daf2c87c34f9a2770d0057d5108
knknkn1162 <knknkn1162@gmail.com>
2018-01-23 17:57:17 +0900

    create new factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,11 @@ FactoryBot.define do
   factory :user do
     name 'user_name'
     email 'example@gmail.com'
+
+    # TODO: confirm the right way to generate fixtures
+    factory :other do
+      name 'other_name'
+      sequence(:email) { |n| "example_#{n}@gmail.com" }
+    end
   end
 end
