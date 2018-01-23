2ebf2917fba0ed86922b1780911ead906f923b00
knknkn1162 <knknkn1162@gmail.com>
2018-01-23 19:33:22 +0900

    display /show and test model

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe 'users/show', type: :view do
       expect(rendered).to have_title full_title(build(:user).name)
     end
   end
+
+  context 'renders users/show only' do
+    before(:each) do
+      user = build(:user)
+      assign(:user, user)
+    end
+
+    it 'displays img and user_name' do
+      user = build(:user)
+      render
+      expect(rendered).to have_selector("img[alt='#{user.name}']",
+                                        class: 'gravatar')
+      expect(rendered).to have_content(user.name)
+    end
+  end
 end
