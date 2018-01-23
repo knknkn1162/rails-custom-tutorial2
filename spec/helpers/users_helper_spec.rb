3d4a54c7d377f2ece86313e771740e13cf88eef4
knknkn1162 <knknkn1162@gmail.com>
2018-01-23 19:25:14 +0900

    define gravatar_for and helper test

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,15 +1,10 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the UsersHelper. For example:
-#
-# describe UsersHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe UsersHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'test gravatar_for' do
+    it 'returns img tag' do
+      user = build(:user)
+      expect(helper.gravatar_for(user)).to have_selector 'img', class: 'gravatar'
+    end
+  end
 end
