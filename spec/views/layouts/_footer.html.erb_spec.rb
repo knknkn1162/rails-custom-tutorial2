abd35c6bfe65232153306c8c388090d3bd6cbd95
knknkn1162 <knknkn1162@gmail.com>
2018-01-23 11:02:53 +0900

    modify routes and set path & test views

--- spec/views/layouts/_footer.html.erb_spec.rb
+++ spec/views/layouts/_footer.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'layouts/_footer', type: :view do
   it 'displays nav link' do
     render
-    expect(rendered).to have_link 'About'
-    expect(rendered).to have_link 'Contact'
+    expect(rendered).to have_link 'About', href: '/about'
+    expect(rendered).to have_link 'Contact', href: '/contact'
     expect(rendered).to have_link 'News'
   end
 end
