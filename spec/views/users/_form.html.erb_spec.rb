f795eb4b25e8eb471cc6067927ba9140cb92923c
knknkn1162 <knknkn1162@gmail.com>
2018-01-24 12:42:09 +0900

    test views _form & new

---
+++ spec/views/users/_form.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "users/_form", type: :view do
+  it 'displays title' do
+    render partial: 'users/form', locals: { model: build(:user) }
+
+    expect(rendered).to have_selector('input[type=email][name="user[email]"]')
+    expect(rendered).to have_selector('input[type=text][name="user[name]"]')
+    expect(rendered).to have_selector('input[type=password][name="user[password]"]')
+    expect(rendered).to have_selector('input[type=password][name="user[password_confirmation]"]')
+
+    expect(rendered).to have_button(count: 1)
+  end
+end
