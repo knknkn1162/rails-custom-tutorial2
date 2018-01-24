e4ca2b8e4d3951faa5766027e8a0cda4986dd2ff
knknkn1162 <knknkn1162@gmail.com>
2018-01-24 11:48:04 +0900

    generate signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  describe "GET #new" do
-    it "returns http success" do
+  describe 'GET #new' do
+    it 'returns http success' do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe 'GET #show' do
+  end
 end
