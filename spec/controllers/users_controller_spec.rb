607709e060fa9f69f6ce0451c0b5b5a3bd12afb2
knknkn1162 <knknkn1162@gmail.com>
2018-01-24 14:11:38 +0900

    controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,12 +3,27 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe 'GET #new' do
+    before(:each) { get :new }
     it 'returns http success' do
-      get :new
       expect(response).to have_http_status(:success)
+    end
+
+    it 'generate new user' do
+      expect(assigns(:user)).to be_a_new(User)
     end
   end
 
   describe 'GET #show' do
+    let(:user) { create(:user) }
+    before {
+      get :show, params: { id: user.id }, session: {}
+    }
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'assigns @user' do
+      expect(assigns(:user)).to eq user
+    end
   end
 end
