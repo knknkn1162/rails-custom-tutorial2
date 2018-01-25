14546d10b2cabaae83a581507749a2e1b883571c
knknkn1162 <knknkn1162@gmail.com>
2018-01-25 14:44:59 +0900

    impl #create and controller test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,59 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  describe "GET #new" do
-    it "returns http success" do
+  describe 'GET #new' do
+    it 'returns http success' do
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe 'POST #create if success' do
+    let(:post_user) {
+      create(:user) do |user|
+        post :create, params: { session: {
+          email: user.email,
+          password: user.password
+        } }
+      end
+    }
+    it 'saves new user' do
+      expect {
+        post_user
+      }.to change(User, :count).by(1)
+    end
+
+    it 'redirects the :create template if success' do
+      user = post_user
+      expect(response).to have_http_status(:redirect)
+      expect(response).to redirect_to(user_path(user))
+    end
+
+    it 'doesnt flash' do
+      post_user
+      expect(flash).to be_empty
+    end
+  end
+
+  describe 'POST #create if failure' do
+    let(:login_invalid_user) do
+      create(:user) do |user|
+        post :create, params: { session: {
+          email: 'dummy@password.com',
+          password: user.password
+        } }
+      end
+    end
+
+    it 'renders the sessions/new template' do
+      login_invalid_user
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template('sessions/new')
+    end
+
+    it 'flash danger' do
+      login_invalid_user
+      expect(flash[:danger]).to be
+    end
+  end
 end
