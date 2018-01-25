ee7cd1f788e71495b38c1339ff5bbf341dfa20a7
knknkn1162 <knknkn1162@gmail.com>
2018-01-25 17:21:28 +0900

    add log_in in #create & test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,63 +9,84 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
-  describe 'POST #create if success' do
-    let(:post_user) {
-      create(:user) do |user|
-        post :create, params: { session: {
-          email: user.email,
-          password: user.password
-        } }
+  describe 'POST #create' do
+    describe 'if success' do
+      let(:post_user) {
+        create(:user) do |user|
+          post :create, params: { session: {
+            email: user.email,
+            password: user.password
+          } }
+        end
+      }
+      it 'saves new user' do
+        expect {
+          post_user
+        }.to change(User, :count).by(1)
       end
-    }
-    it 'saves new user' do
-      expect {
+
+      it 'redirects the :create template if success' do
+        user = post_user
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to("/users/#{user.id}")
+      end
+
+      it 'doesnt flash' do
         post_user
-      }.to change(User, :count).by(1)
-    end
+        expect(flash).to be_empty
+      end
 
-    it 'redirects the :create template if success' do
-      user = post_user
-      expect(response).to have_http_status(:redirect)
-      expect(response).to redirect_to(user_path(user))
-    end
-
-    it 'doesnt flash' do
-      post_user
-      expect(flash).to be_empty
-    end
-  end
-
-  describe 'POST #create if email failure' do
-    let(:login_invalid_user) do
-      create(:user) do |user|
-        post :create, params: { session: {
-          email: 'dummy@password.com',
-          password: user.password
-        } }
+      it 'contains user_id in session' do
+        user = post_user
+        expect(session[:user_id]).to eq user.id
       end
     end
 
-    it 'renders the sessions/new template' do
-      login_invalid_user
-      expect(response).to have_http_status(:success)
-      expect(response).to render_template('sessions/new')
+    describe 'if email failure' do
+      let(:login_invalid_user) do
+        create(:user) do |user|
+          post :create, params: { session: {
+            email: 'dummy@password.com',
+            password: user.password
+          } }
+        end
+      end
+
+      it 'renders the sessions/new template' do
+        login_invalid_user
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('sessions/new')
+      end
+
+      it 'flash danger' do
+        login_invalid_user
+        expect(flash[:danger]).to be
+      end
     end
 
-    it 'flash danger' do
-      login_invalid_user
-      expect(flash[:danger]).to be
+    describe 'if password failure' do
+      let(:login_invalid_user) do
+        create(:user) do |user|
+          post :create, params: { session: {
+            email: user.email,
+            password: user.password + 'dummy'
+          } }
+        end
+      end
+
+      it 'renders the sessions/new template' do
+        login_invalid_user
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template('sessions/new')
+      end
+
+      it 'flash danger' do
+        login_invalid_user
+        expect(flash[:danger]).to be
+      end
     end
   end
 
-  describe 'POST #create if password failure' do
-    let(:login_invalid_user) do
-      create(:user) do |user|
-        post :create, params: { session: {
-          email: user.email,
-          password: user.password + 'dummy'
-        } }
-      end
     end
 
     it 'renders the sessions/new template' do
