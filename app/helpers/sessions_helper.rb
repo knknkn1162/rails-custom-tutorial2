a50c4755c5eecd39544171bd331a77b300efdced
knknkn1162 <knknkn1162@gmail.com>
2018-01-27 21:22:01 +0900

    add forget_log_in_session method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,10 @@ module SessionsHelper
     session[:user_id]
   end
 
+  def forget_log_in_session
+    session.delete(:user_id)
+  end
+
   # TODO: how to deal with cookies? Should not be contains any method in helper module due to rspec test?
   def get_user(user_id)
     User.find_by(id: user_id)
--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SessionsHelper, type: :helper do
   describe 'set_log_in_session test' do
     it 'tests set_log_in' do
       user = instance_double('User', id: 10)
-      helper.set_log_in_session(user)
+      set_log_in_session(user)
       expect(session[:user_id]).to eq 10
     end
   end
@@ -13,6 +13,14 @@ RSpec.describe SessionsHelper, type: :helper do
     it 'tests get_log_in' do
       session[:user_id] = 100
       expect(get_log_in_session).to eq 100
+    end
+  end
+
+  describe 'forget_log_in_session test' do
+    it 'tests forget_log_in_session' do
+      session[:user_id] = 100
+      forget_log_in_session
+      expect(session).to be_empty
     end
   end
 
