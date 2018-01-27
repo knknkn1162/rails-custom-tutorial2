0a25fd28fca3692ccceb5378f7e9725fd78e01f5
knknkn1162 <knknkn1162@gmail.com>
2018-01-27 09:10:41 +0900

    modify cookies string key

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,10 @@ class SessionsController < ApplicationController
     if @user&.authenticate(params[:session][:password])
       log_in @user
       @user.remember
-      cookies.permanent.signed[:user_id] = @user.id
-      cookies.permanent[:remember_token] = @user.remember_token
+      # :foo and "foo" are different keys in controller spec. To avoid inconsistancy, we use string keys
+      # see also, `Why not use the cookies method?` section in https://relishapp.com/rspec/rspec-rails/docs/controller-specs/cookies.
+      cookies.permanent.signed['user_id'] = @user.id
+      cookies.permanent['remember_token'] = @user.remember_token
       redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
