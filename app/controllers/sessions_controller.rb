9c9906838f72d2c5cedb2f823a7921588cf29c57
knknkn1162 <knknkn1162@gmail.com>
2018-01-27 21:38:17 +0900

    refactor sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,8 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
-      log_in @user
-      @user.remember
-      # :foo and "foo" are different keys in controller spec. To avoid inconsistancy, we use string keys
-      # see also, `Why not use the cookies method?` section in https://relishapp.com/rspec/rspec-rails/docs/controller-specs/cookies.
-      cookies.permanent.signed['user_id'] = @user.id
-      cookies.permanent['remember_token'] = @user.remember_token
+      set_log_in_session @user
+      params[:session][:remember_me] == '1' ? remember : forget
       redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -19,20 +15,14 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    user = current_user
-    if user
-      # clear remember_digest
-      user.forget
-      cookies.delete('user_id')
-      cookies.delete('remember_token')
-      session.delete(:user_id)
-    end
+    @user = current_user
+    forget || forget_log_in_session if @user
     redirect_to root_url
   end
 
+  # REVIEW: Doesn't this method have to be tested in spec/contorollers?
   private
 
-  # REVIEW: Doesn't this method have to be tested in spec/contorollers?
   def current_user
     user_id = get_log_in_session
     if user_id
@@ -44,5 +34,19 @@ class SessionsController < ApplicationController
         user
       end
     end
+  end
+
+  def remember
+    @user.remember
+    # :foo and "foo" are different keys in controller spec. To avoid inconsistancy, we use string keys
+    # see also, `Why not use the cookies method?` section in https://relishapp.com/rspec/rspec-rails/docs/controller-specs/cookies.
+    cookies.permanent.signed['user_id'] = @user.id
+    cookies.permanent['remember_token'] = @user.remember_token
+  end
+
+  def forget
+    @user.forget
+    cookies.delete('user_id')
+    cookies.delete('remember_token')
   end
 end
