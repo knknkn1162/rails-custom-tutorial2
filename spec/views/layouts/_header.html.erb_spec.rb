7abe206993a77fe025e53de32f2002b2357e860d
knknkn1162 <knknkn1162@gmail.com>
2018-01-29 11:36:31 +0900

    test dropdown layouts/_header view

--- spec/views/layouts/_header.html.erb_spec.rb
+++ spec/views/layouts/_header.html.erb_spec.rb
@@ -1,11 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "layouts/_header", type: :view do
-  it 'displays link' do
-    render
-    expect(rendered).to have_link href: '/', count: 2
-    expect(rendered).to have_link 'Help', href: '/help'
-    expect(rendered).to have_link 'Log in', href: '/login'
-    expect(rendered).to have_selector('a#logo')
+  describe 'render html' do
+    it 'displays link' do
+      render
+      expect(rendered).to have_link href: '/', count: 2
+      expect(rendered).to have_link 'Help', href: '/help'
+      expect(rendered).to have_selector('a#logo')
+    end
+  end
+
+  describe 'when logged_in? returns true' do
+    let(:logged_in_true) do
+      allow(view).to receive(:logged_in?).and_return(true)
+      allow(view).to receive(:current_user).and_return(create(:user))
+    end
+
+    it 'renders dropdown partial' do
+      logged_in_true
+      render
+      expect(rendered).to render_template partial: 'layouts/_dropdown'
+    end
+  end
+
+  describe 'when logged_in? returns false' do
+    let(:logged_in_false) do
+      allow(view).to receive(:logged_in?).and_return(false)
+    end
+
+    it 'renders login link' do
+      logged_in_false
+      render
+      expect(rendered).to have_link 'Log in', href: '/login'
+    end
   end
 end
