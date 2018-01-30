56317de4afd65daeb529bd0a09e89efad0f3c786
knknkn1162 <knknkn1162@gmail.com>
2018-01-30 10:56:16 +0900

    test index view

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,32 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe 'users/index', type: :view do
-  before do
-    create_list(:other, 31)
-    # NOTE: default users per-page is 30
-    assign(:users, User.paginate(page: 1, per_page: 20))
-  end
 
-  it 'displays title' do
-    render template: 'users/index', layout: 'layouts/application'
-    expect(rendered).to have_title full_title('All users')
-  end
+  let!(:others) { create_list(:other, 31) }
 
-  it 'renders form partial' do
-    render
-    expect(rendered).to render_template partial: 'users/_user'
-  end
+  describe 'when pagination doesnt render' do
+    before do
+      # NOTE: default users per-page is 30
+      assign(:users, User.paginate(page: 1, per_page: 50))
+    end
 
-  it 'displays pagination' do
-    render
-    expect(rendered).to have_selector('div.pagination', count: 2)
-  end
+    it 'displays title' do
+      render template: 'users/index', layout: 'layouts/application'
+      expect(rendered).to have_title full_title('All users')
+    end
 
-  describe 'users/_user', type: :view do
-    it 'renders list of 20, users' do
+    it 'renders form partial' do
       render
-      expect(rendered).to have_selector('ul.users li', count: 20)
-      expect(rendered).to have_selector('ul.users a', count: 20)
+      expect(rendered).to render_template partial: 'users/_user'
+    end
+
+    it 'displays pagination' do
+      render
+      expect(rendered).not_to have_selector('div.pagination')
+    end
+
+    describe 'users/_user', type: :view do
+      it 'renders list of 20, users' do
+        render
+        expect(rendered).to have_selector('ul.users li', count: 31)
+        expect(rendered).to have_selector('ul.users a', count: 31)
+      end
+    end
+  end
+
+  describe 'when pagination renders' do
+    before do
+      # NOTE: default users per-page is 30
+      assign(:users, User.paginate(page: 1, per_page: 20))
+    end
+
+    it 'displays pagination' do
+      render
+      expect(rendered).to have_selector('div.pagination', count: 2)
+    end
+
+    describe 'users/_user', type: :view do
+      it 'renders list of 20, users' do
+        render
+        expect(rendered).to have_selector('ul.users li', count: 20)
+        expect(rendered).to have_selector('ul.users a', count: 20)
+      end
     end
   end
 end
