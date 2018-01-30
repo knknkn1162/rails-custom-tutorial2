57b51843ff52f5aa2d3a17677f3f4f477fbb248d
knknkn1162 <knknkn1162@gmail.com>
2018-01-30 19:33:35 +0900

    bugfix users/_user partial and test

--- spec/views/users/_user.html.erb_spec.rb
+++ spec/views/users/_user.html.erb_spec.rb
@@ -2,37 +2,54 @@ require 'rails_helper'
 
 RSpec.describe 'users/_user', type: :view do
   let(:user) { create(:user) }
-  let(:current_user_false) do
-    allow(view).to receive(:current_user?).and_return(false)
+  let(:other) { create(:other) }
+  let(:stubbed_current_user) do
+    allow(view).to receive(:current_user).and_return(current_user_flag)
+  end
+
+  let(:current_user_ok) do
+    # it indicates that current_user.admin equals true
+    allow(view).to receive(:current_user).and_return(user)
   end
 
   it 'renders link and img' do
+    current_user_ok
     render 'users/user', user: user
     expect(rendered).to have_selector('img.gravatar')
     expect(rendered).to have_link(href: "/users/#{user.id}", text: user.name)
   end
 
-  describe 'when admin user' do
-    it 'renders delete link' do
-      current_user_false
-      render 'users/user', user: user
+  describe 'when current_user is user' do
+    let(:current_user_flag) { user }
+    it 'renders others delete link' do
+      stubbed_current_user
+      render 'users/user', user: other
       expect(rendered).to have_link(
+        href: "/users/#{other.id}",
+        text: 'delete'
+      )
+    end
+
+    it 'doesnt render users delete link' do
+      stubbed_current_user
+      render 'users/user', user: user
+      expect(rendered).not_to have_link(
         href: "/users/#{user.id}",
         text: 'delete'
       )
     end
   end
 
-  describe 'when non-admin user' do
-    let(:other) { create(:other) }
-    let(:current_user_true) do
-      allow(view).to receive(:current_user?).and_return(true)
-    end
-
-    it 'doesnt exist delete link' do
-      current_user_true
+  describe 'when current_user is other' do
+    let(:current_user_flag) { other }
+    it 'renders delelte link' do
+      stubbed_current_user
       render 'users/user', user: other
-      expect(rendered).not_to have_link(text: 'delete')
+
+      expect(rendered).not_to have_link(
+        href: "/users/#{other.id}",
+        text: 'delete'
+      )
     end
   end
 end
