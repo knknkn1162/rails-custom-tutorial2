0c1c4f8c38b0fd92d81827789158eb538a52175b
knknkn1162 <knknkn1162@gmail.com>
2018-01-30 13:20:32 +0900

    set delete tag in users/_user and test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,11 +4,13 @@ FactoryBot.define do
     email 'example@gmail.com'
     password 'foobar'
     password_confirmation 'foobar'
+    admin true
 
     # TODO: confirm the right way to generate fixtures
     factory :other do
       sequence(:name) { |n| "other_name-#{n}" }
       sequence(:email) { |n| "example_#{n}@gmail.com" }
+      admin false
     end
   end
 end
--- spec/views/users/_user.html.erb_spec.rb
+++ spec/views/users/_user.html.erb_spec.rb
@@ -2,9 +2,37 @@ require 'rails_helper'
 
 RSpec.describe 'users/_user', type: :view do
   let(:user) { create(:user) }
-  it 'renders ' do
+  let(:current_user_false) do
+    allow(view).to receive(:current_user?).and_return(false)
+  end
+
+  it 'renders link and img' do
     render 'users/user', user: user
     expect(rendered).to have_selector('img.gravatar')
     expect(rendered).to have_link(href: "/users/#{user.id}", text: user.name)
+  end
+
+  describe 'when admin user' do
+    it 'renders delete link' do
+      current_user_false
+      render 'users/user', user: user
+      expect(rendered).to have_link(
+        href: "/users/#{user.id}",
+        text: 'delete'
+      )
+    end
+  end
+
+  describe 'when non-admin user' do
+    let(:other) { create(:other) }
+    let(:current_user_true) do
+      allow(view).to receive(:current_user?).and_return(true)
+    end
+
+    it 'doesnt exist delete link' do
+      current_user_true
+      render 'users/user', user: other
+      expect(rendered).not_to have_link(text: 'delete')
+    end
   end
 end
