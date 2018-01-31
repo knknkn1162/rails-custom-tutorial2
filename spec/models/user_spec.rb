fef71496e7761b2995414ffae44997d90b6fa746
knknkn1162 <knknkn1162@gmail.com>
2018-01-31 11:08:10 +0900

    refactor model/user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,90 +1,87 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'when invalid name contains' do
-    it 'should be non-empty name' do
-      user = build(:user, name: ' ')
-      expect(user).not_to be_valid
-    end
+  describe 'when validation check' do
+    context 'when invalid name contains' do
+      it 'should be non-empty name' do
+        user = build(:user, name: ' ')
+        expect(user).not_to be_valid
+      end
 
-    it 'should not be long name' do
-      user = build(:user, name: 'a' * 51)
-      expect(user).not_to be_valid
-    end
-  end
-
-  context 'when the invalid email contains' do
-    it 'should be non-empty email' do
-      user = build(:user, email: ' ')
-      expect(user).not_to be_valid
-    end
-
-    it 'should not be long email' do
-      suffix = '@example.com'
-      user = build(:user, email: 'a' * (256 - suffix.length) + suffix)
-      expect(user).not_to be_valid
-    end
-
-    it 'should be email-formatted' do
-      %w[user@example,com
-         user_at_foo.org
-         user.name@example.foo@bar_baz.com
-         foo@bar+baz.com].each do |address|
-        user = build(:user, email: address)
+      it 'should not be long name' do
+        user = build(:user, name: 'a' * 51)
         expect(user).not_to be_valid
       end
     end
 
-    it 'should be unique email' do
-      create(:user)
-      duplicated_user = build(:user)
-      expect(duplicated_user).not_to be_valid
-    end
-  end
+    context 'when the invalid email contains' do
+      it 'should be non-empty email' do
+        user = build(:user, email: ' ')
+        expect(user).not_to be_valid
+      end
 
-  context 'when the invalid password contains' do
-    it 'should be present(nonblank) password' do
-      user = build(:user, password: ' ' * 6)
-      expect(user).not_to be_valid
+      it 'should not be long email' do
+        suffix = '@example.com'
+        user = build(:user, email: 'a' * (256 - suffix.length) + suffix)
+        expect(user).not_to be_valid
+      end
+
+      it 'should be email-formatted' do
+        %w[user@example,com
+          user_at_foo.org
+          user.name@example.foo@bar_baz.com
+          foo@bar+baz.com].each do |address|
+          user = build(:user, email: address)
+          expect(user).not_to be_valid
+        end
+      end
+
+      it 'should be unique email' do
+        create(:user)
+        duplicated_user = build(:user)
+        expect(duplicated_user).not_to be_valid
+      end
     end
 
-    it 'should be at least 6 characters' do
-      invalid_password = 'a' * 5
-      user = build(:user, password: invalid_password, password_confirmation: 5)
-      expect(user).not_to be_valid
-    end
-  end
+    context 'when the invalid password contains' do
+      it 'should be present(nonblank) password' do
+        user = build(:user, password: ' ' * 6)
+        expect(user).not_to be_valid
+      end
 
-  context 'when the valid email contains' do
-    it 'should be email-formatted' do
-      %w[user@example.com
-         USER@foo.COM
-         A_US-ER@foo.bar.org
-         first.last@foo.jp
-         alice+bob@baz.cn].each do |address|
-        user = build(:user, email: address)
-        expect(user).to be_valid
+      it 'should be at least 6 characters' do
+        invalid_password = 'a' * 5
+        user = build(:user, password: invalid_password, password_confirmation: 5)
+        expect(user).not_to be_valid
+      end
+    end
+
+    context 'when the valid email contains' do
+      it 'should be email-formatted' do
+        %w[user@example.com
+          USER@foo.COM
+          A_US-ER@foo.bar.org
+          first.last@foo.jp
+          alice+bob@baz.cn].each do |address|
+          user = build(:user, email: address)
+          expect(user).to be_valid
+        end
       end
     end
   end
 
-  context 'when callbacks' do
-    let(:user) { create(:user) }
-    it 'calls downcase_email before_save' do
-      expect(user).to callback(:downcase_email).before(:save)
-    end
-  end
-
-  context 'when public method calls' do
+  describe 'when public method calls' do
     include UsersHelper
     it 'tests remember method' do
       user = build(:user)
       user.remember
       expect(user.remember_token).to be
-      expect(BCrypt::Password.new(user.remember_digest)).to eq user.remember_token
+      expect(
+        BCrypt::Password.new(user.remember_digest)
+      ).to eq user.remember_token
     end
 
-    describe 'tests authenticated? method' do
+    context 'tests authenticated? method' do
       it 'fails when not remembered' do
         user = build(:user)
         expect(user.authenticated?(nil)).to be_falsy
@@ -104,7 +101,7 @@ RSpec.describe User, type: :model do
       end
     end
 
-    describe 'when forget method' do
+    context 'when forget method' do
       it 'clear remember_digest' do
         user = build(:user, remember_digest: 'sample')
         user.forget
@@ -112,11 +109,19 @@ RSpec.describe User, type: :model do
       end
     end
   end
-  context 'when private method calls' do
+
+  describe 'when private method calls' do
     it 'downcases email' do
       user = build(:user, email: 'Foo@ExAmPLe.COm')
       user.send(:downcase_email)
       expect(user.email).to eq 'foo@example.com'
+    end
+  end
+
+  describe 'when callbacks' do
+    let(:user) { create(:user) }
+    it 'calls downcase_email before_save' do
+      expect(user).to callback(:downcase_email).before(:save)
     end
   end
 end
