1d8c7ecd4ce9c96faccae0297609ce47c14f8f62
knknkn1162 <knknkn1162@gmail.com>
2018-01-31 12:46:54 +0900

    add activated in factory and seeds

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,12 +5,15 @@ FactoryBot.define do
     password 'foobar'
     password_confirmation 'foobar'
     admin true
+    acticated true
+    activated_at { Time.zone.now }
 
     # TODO: confirm the right way to generate fixtures
     factory :other do
       sequence(:name) { |n| "other_name-#{n}" }
       sequence(:email) { |n| "example_#{n}@gmail.com" }
       admin false
+      sequence(:activated_at) { Time.zone.now }
     end
   end
 end
