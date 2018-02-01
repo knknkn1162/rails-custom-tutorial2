80333ed9dc20695e09211b88eefc9472dfd55c70
knknkn1162 <knknkn1162@gmail.com>
2018-02-01 13:43:41 +0900

    add activate & in send_activation_email in User and refactor

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,10 +2,7 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if !user&.activated? && user&.authenticated?(:activation, params[:id])
-      user.update_attributes(
-        activated: true,
-        activated_at: Time.zone.now
-      )
+      user.activate
       set_log_in_session(user)
       flash[:success] = 'Account activated!'
       redirect_to user
