055852b7de60cd9ac6d5f8b3fcc983862cd66629
knknkn1162 <knknkn1162@gmail.com>
2018-02-02 13:36:16 +0900

    impl edit & test

--- spec/views/password_resets/edit.html.erb_spec.rb
+++ spec/views/password_resets/edit.html.erb_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "password_resets/edit.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'password_resets/edit.html.erb', type: :view do
+  before(:each) do
+    assign(:user, build(:user))
+    assign(:token_id, 'sample_token')
+  end
+  it 'displays title' do
+    render template: 'password_resets/edit', layout: 'layouts/application'
+    expect(rendered).to have_title full_title('Reset password')
+  end
+
+  it 'renders form partial' do
+    render
+    expect(rendered).to render_template partial: 'shared/_error_messages'
+  end
+
+  it 'renders input' do
+    render
+    expect(rendered).to have_selector('input[type=password][name="user[password]"]')
+    expect(rendered).to have_selector('input[type=password][name="user[password_confirmation]"]')
+  end
+
+  it 'renders button' do
+    render
+    expect(rendered).to have_button(value: 'Update password')
+    expect(rendered).to have_button(count: 1)
+  end
 end
