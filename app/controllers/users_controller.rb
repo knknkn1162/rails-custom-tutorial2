0a86162e050cf0845832fe887e383622498e46c3
knknkn1162 <knknkn1162@gmail.com>
2018-02-05 12:46:41 +0900

    add static_page/home when logged_in and refactor spec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,6 @@ class UsersController < ApplicationController
     if @user.save
       @user.send_activation_email
       flash[:info] = 'Please check your email to activate your account'
-      # same as user_url(@user)
       redirect_to root_url
     else
       render 'new'
--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "static_pages/home.html.erb", type: :view do
-  it 'displays title' do
-    render template: 'static_pages/home', layout: 'layouts/application'
-    expect(rendered).to have_title full_title('Home')
+RSpec.describe 'static_pages/home.html.erb', type: :view do
+  let(:stubbed_logged_in) do
+    allow(view).to receive(:logged_in?).and_return(logged_in_flag)
   end
 
-  it 'displays sign up now' do
-    render
-    expect(rendered).to have_link href: '/signup'
+  # default
+  let(:logged_in_flag) { false }
+  describe 'when not logged in' do
+    it 'displays title' do
+      render template: 'static_pages/home', layout: 'layouts/application'
+      expect(rendered).to have_title full_title('Home')
+    end
+
+    it 'displays sign up now' do
+      render
+      expect(rendered).to have_link href: '/signup'
+    end
   end
 end
