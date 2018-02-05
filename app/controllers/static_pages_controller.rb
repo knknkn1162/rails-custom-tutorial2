97f61eef2e4d2db24814f7fc20dd23d2c16a5c50
knknkn1162 <knknkn1162@gmail.com>
2018-02-05 14:26:00 +0900

    add micropost instance-variable in StaticPages#home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
+  include UsersHelper
   def home
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,9 +2,35 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
   describe 'GET #home' do
-    it 'returns http success' do
+    let(:stubbed_current_user) do
+      allow(controller).to receive(:logged_in?).and_return(logged_in_flag)
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+
+    let(:user) { create(:user) }
+
+    before(:each) do
+      stubbed_current_user
       get :home
-      expect(response).to have_http_status(:success)
+    end
+
+    describe 'when not logged_in' do
+      let(:logged_in_flag) { false }
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'doesnt assign micropost' do
+        expect(assigns(:micropost)).not_to be
+      end
+    end
+
+    describe 'when logged_in' do
+      let(:logged_in_flag) { true }
+
+      it 'assigns micropost' do
+        expect(assigns(:micropost)).to be
+      end
     end
   end
 
