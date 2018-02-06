6be567b34c053a7c83872d7d18217c56925938c4
knknkn1162 <knknkn1162@gmail.com>
2018-02-06 14:55:44 +0900

    refactor user/new view spec

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -2,16 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'users/new.html.erb', type: :view do
   before(:each) do
-    assign(:user, build(:user))
-  end
-  it 'displays title' do
-    render template: 'users/new', layout: 'layouts/application'
-    expect(rendered).to have_title full_title('Sign up')
+    inject_user
+    render template: 'users/new', layout: layout
   end
 
-  it 'renders form partial' do
-    render
-    expect(rendered).to render_template partial: 'users/_form'
-    expect(rendered).to render_template partial: 'shared/_error_messages'
+  let(:inject_user) { assign(:user, build(:user)) }
+  let(:layout) { false }
+
+  context 'when layout includes in layouts/application directory' do
+    let(:layout) { 'layouts/application' }
+    it 'has correct title' do
+      expect(rendered).to have_title full_title('Sign up')
+    end
+  end
+
+  context 'when no layouts' do
+    it 'renders partial pages' do
+      ['users/_form', 'shared/_error_messages'].each do |partial|
+        expect(rendered).to render_template partial: partial
+      end
+    end
   end
 end
