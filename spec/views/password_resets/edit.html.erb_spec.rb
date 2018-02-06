c8dffa50014e75b7120177ac3c38ad865929e5d7
knknkn1162 <knknkn1162@gmail.com>
2018-02-06 19:45:38 +0900

    refactor password_resets/edit view spec

--- spec/views/password_resets/edit.html.erb_spec.rb
+++ spec/views/password_resets/edit.html.erb_spec.rb
@@ -2,26 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'password_resets/edit.html.erb', type: :view do
   before(:each) do
+    inject_user
+    render template: 'password_resets/edit', layout: layout
+  end
+
+  let(:inject_user) do
     assign(:user, build(:user, reset_token: 'sample_token'))
   end
-  it 'displays title' do
-    render template: 'password_resets/edit', layout: 'layouts/application'
-    expect(rendered).to have_title full_title('Reset password')
+
+  # default
+  let(:layout) { false }
+
+  context 'when renders layout' do
+    let(:layout) { 'layouts/application' }
+    it 'displays title' do
+      expect(rendered).to have_title full_title('Reset password')
+    end
   end
 
   it 'renders form partial' do
-    render
     expect(rendered).to render_template partial: 'shared/_error_messages'
   end
 
   it 'renders input' do
-    render
     expect(rendered).to have_selector('input[type=password][name="user[password]"]')
     expect(rendered).to have_selector('input[type=password][name="user[password_confirmation]"]')
   end
 
   it 'renders button' do
-    render
     expect(rendered).to have_button(value: 'Update password')
     expect(rendered).to have_button(count: 1)
   end
