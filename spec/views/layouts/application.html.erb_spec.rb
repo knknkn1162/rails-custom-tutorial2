99e6c533a28d053cda383af1a1e1142a653db061
knknkn1162 <knknkn1162@gmail.com>
2018-02-06 22:53:43 +0900

    refactor application view spec

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -1,21 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "layouts/application", type: :view do
-  it 'displays title' do
-    page_title = 'Sample'
-    # TODO: it seems to be better!
-    inline = "<% provide(:title, '#{page_title}') %>"
+RSpec.describe 'layouts/application', type: :view do
+  before(:each) do
+    store_flashes
     render inline: inline, layout: 'layouts/application'
-
-    expect(rendered).to have_title full_title(page_title)
   end
 
-  it 'displays single flash' do
-    test_sentence = 'This is a test'
-    flash[:test1] = test_sentence + '1'
-    flash[:test2] = test_sentence + '2'
-    render inline: '', layout: 'layouts/application'
-    expect(rendered).to have_selector 'div.alert-test1', text: test_sentence + '1'
-    expect(rendered).to have_selector 'div.alert-test2', text: test_sentence + '2'
+  # default
+  let(:inline) { '' }
+  let(:store_flashes) {}
+
+  context 'when title is provided' do
+    let(:page_title) { 'Sample' }
+    let(:inline) { "<% provide(:title, '#{page_title}') %>" }
+    it 'displays title' do
+      expect(rendered).to have_title /#{page_title}/
+    end
+  end
+
+  context 'when provide nothing' do
+    let(:sentences) { Faker::Lorem.sentences(2) }
+    let(:store_flashes) do
+      flash[:test1] = sentences[0]
+      flash[:test2] = sentences[1]
+    end
+
+    it 'displays flashes' do
+      expect(rendered).to have_selector 'div.alert-test1', text: sentences[0]
+      expect(rendered).to have_selector 'div.alert-test2', text: sentences[1]
+    end
   end
 end
