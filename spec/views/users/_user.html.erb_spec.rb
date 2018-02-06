ab67425a787cf8c6b1d1e78bf1a8d5501222e3d9
knknkn1162 <knknkn1162@gmail.com>
2018-02-06 17:27:05 +0900

    refactor users/_user view spec

--- spec/views/users/_user.html.erb_spec.rb
+++ spec/views/users/_user.html.erb_spec.rb
@@ -1,57 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe 'users/_user', type: :view do
+  before(:each) do
+    stubbed_current_user
+    render 'users/user', user: local_user
+  end
+
   let(:user) { create(:user) }
   let(:other) { create(:other) }
-  # current_user? and admin_current_user? depends on helper.current_user method
-  # The method chain expression seems to be invalid!
+
   let(:stubbed_current_user) do
     allow(view).to receive(:current_user).and_return(current_user_flag)
   end
 
-  let(:current_user_ok) do
-    # it indicates that current_user.admin equals true
-    allow(view).to receive(:current_user).and_return(user)
+  # default
+  let(:current_user_flag) { user }
+  let(:local_user) { user }
+
+  # expr
+  let(:have_user_delete_link) do
+    have_link(href: "/users/#{user.id}", text: 'delete')
+  end
+  let(:have_other_delete_link) do
+    have_link(href: "/users/#{other.id}", text: 'delete')
   end
 
   it 'renders link and img' do
-    current_user_ok
-    render 'users/user', user: user
     expect(rendered).to have_selector('img.gravatar')
     expect(rendered).to have_link(href: "/users/#{user.id}", text: user.name)
   end
 
-  describe 'when current_user is user' do
-    let(:current_user_flag) { user }
+  context 'when current_user is user' do
+    let(:local_user) { other }
     it 'renders others delete link' do
-      stubbed_current_user
-      render 'users/user', user: other
-      expect(rendered).to have_link(
-        href: "/users/#{other.id}",
-        text: 'delete'
-      )
+      expect(rendered).to have_other_delete_link
     end
 
     it 'doesnt render users delete link' do
-      stubbed_current_user
-      render 'users/user', user: user
-      expect(rendered).not_to have_link(
-        href: "/users/#{user.id}",
-        text: 'delete'
-      )
+      expect(rendered).not_to have_user_delete_link
     end
   end
 
-  describe 'when current_user is other' do
+  context 'when current_user is other' do
     let(:current_user_flag) { other }
     it 'renders delelte link' do
-      stubbed_current_user
-      render 'users/user', user: other
-
-      expect(rendered).not_to have_link(
-        href: "/users/#{other.id}",
-        text: 'delete'
-      )
+      expect(rendered).not_to have_other_delete_link
+      expect(rendered).not_to have_user_delete_link
     end
   end
 end
