2076150a02670d2dd4677c3b9dd942eeae24af67
knknkn1162 <knknkn1162@gmail.com>
2018-02-06 19:49:56 +0900

    password_resets/new view spec

--- spec/views/password_resets/new.html.erb_spec.rb
+++ spec/views/password_resets/new.html.erb_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "password_resets/new.html.erb", type: :view do
+RSpec.describe 'password_resets/new.html.erb', type: :view do
+  before(:each) do
+    render template: 'password_resets/new', layout: layout
+  end
+  let(:layout) { false }
+
   context 'renders layouts' do
+    let(:layout) { 'layouts/application' }
     it 'displays img and user_name' do
       render template: 'password_resets/new', layout: 'layouts/application'
-      expect(rendered).to have_title full_title('Forget password')
+      expect(rendered).to have_title(/Forget password/)
     end
   end
 
   context 'displays html' do
-    before(:each) { render }
     it 'displays input' do
       expect(rendered).to have_selector('input[type=email][name="password_reset[email]"]')
     end
