310b66b3d3ac47eecd3a34a6418272886eb2fe95
knknkn1162 <knknkn1162@gmail.com>
2018-02-06 18:50:18 +0900

    refactor home page

--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -5,28 +5,34 @@ RSpec.describe 'static_pages/home.html.erb', type: :view do
     allow(view).to receive(:logged_in?).and_return(logged_in_flag)
   end
 
+  let(:render_home) do
+    render template: 'static_pages/home', layout: layout
+  end
+
   # default
   let(:logged_in_flag) { false }
-  describe 'when not logged in' do
+  let(:layout) { false }
+
+  context 'when not logged in' do
+    let(:layout) { 'layouts/application' }
+
     before(:each) do
       stubbed_logged_in
+      render_home
     end
 
     it 'displays title' do
-      render template: 'static_pages/home', layout: 'layouts/application'
-      expect(rendered).to have_title full_title('Home')
+      expect(rendered).to have_title /Home/
     end
 
     it 'displays sign up now' do
-      render
       expect(rendered).to have_link href: '/signup'
     end
   end
 
   describe 'when logged in' do
-    let(:logged_in_flag) { true }
     let(:user) do
-      create(:user_with_microposts, microposts_count: 6)
+      create(:user_with_microposts, microposts_count: per_page + 1)
     end
 
     let(:stubbed_current_user) do
@@ -36,22 +42,27 @@ RSpec.describe 'static_pages/home.html.erb', type: :view do
 
     # instance variable injection
     let(:inject_feed_items) do
-      assign(:feed_items, user.microposts.paginate(page: 1, per_page: 5))
+      assign(:feed_items, user.microposts.paginate(page: 1, per_page: per_page))
     end
 
     let(:inject_micropost) do
       assign(:micropost, Micropost.new)
     end
 
+    # default
+    let(:logged_in_flag) { true }
+    let(:per_page) { 5 }
+
     before(:each) do
       inject_feed_items
       inject_micropost
       stubbed_current_user
       stubbed_logged_in
-      render
+      render_home
     end
+
     it 'renders _log_in_home' do
-      expect(rendered).to render_template('static_pages/_login_home')
+      expect(rendered).to render_template 'static_pages/_login_home'
     end
   end
 end
