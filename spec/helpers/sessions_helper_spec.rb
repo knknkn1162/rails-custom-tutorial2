e352e0e6d27a0f735145052e55951cdea40cd3ba
knknkn1162 <knknkn1162@gmail.com>
2018-02-07 15:02:15 +0900

    refactor sessions_helper_spec

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe SessionsHelper, type: :helper do
   describe 'set_log_in_session' do
     let(:user) { create(:user) }
     before(:each) do
-      set_log_in_session(user)
+      helper.set_log_in_session(user)
     end
     it 'sets user_id hash' do
       expect(session[:user_id]).to eq user.id
@@ -19,7 +19,7 @@ RSpec.describe SessionsHelper, type: :helper do
       session[:user_id] = user_id
     end
     it 'gets user_id hash' do
-      expect(get_log_in_session).to eq user_id
+      expect(helper.get_log_in_session).to eq user_id
     end
   end
 
@@ -27,16 +27,16 @@ RSpec.describe SessionsHelper, type: :helper do
     let(:user_id) { 100 }
     before(:each) do
       session[:user_id] = user_id
-      forget_log_in_session
+      helper.forget_log_in_session
     end
     it 'deletes user_id hash' do
-      expect(session).to be_empty
+      expect(helper.session).to be_empty
     end
   end
 
   describe 'store_location' do
     before(:each) do
-      store_location('/dummy')
+      helper.store_location('/dummy')
     end
     it 'stores forwaring_url hash' do
       expect(session[:forwarding_url]).to eq '/dummy'
@@ -48,14 +48,14 @@ RSpec.describe SessionsHelper, type: :helper do
       session[:forwarding_url] = forwarding_url
     end
     it 'gets forwarding_url hash' do
-      expect(get_location).to eq forwarding_url
+      expect(helper.get_location).to eq forwarding_url
     end
   end
 
   describe 'delete_location test' do
     before(:each) do
       session[:forwarding_url] = forwarding_url
-      delete_location
+      helper.delete_location
     end
     it 'deletes forwarding_url hash' do
       expect(session).to be_empty
