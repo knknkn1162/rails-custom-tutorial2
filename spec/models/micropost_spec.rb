5d78d1af2d03a06e05fb638b54b31f229d696457
knknkn1162 <knknkn1162@gmail.com>
2018-02-07 18:16:16 +0900

    refactor micropost model

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
   let(:user) { create(:user) }
-  describe 'when validation check' do
+  context 'when validation check' do
     let(:micropost) do
       build(:micropost, content: content, user: user)
     end
@@ -15,21 +15,21 @@ RSpec.describe Micropost, type: :model do
     end
 
     context 'when invalid name contains' do
-      describe 'when should have non-empty user ' do
+      context 'when nil user ' do
         let(:user) { nil }
         it 'should have non-empty user' do
           expect(micropost).not_to be_valid
         end
       end
 
-      describe 'content should be present' do
+      context 'when blank content' do
         let(:content) { ' ' }
         it 'should be present' do
           expect(micropost).not_to be_valid
         end
       end
 
-      describe 'context should be at most 140 characters' do
+      context 'when content should be at most 140 characters' do
         let(:content) { 'a' * 141 }
         it 'should be at least 140 characters' do
           expect(micropost).not_to be_valid
@@ -38,7 +38,7 @@ RSpec.describe Micropost, type: :model do
     end
   end
 
-  describe 'order by created_at descendingly' do
+  context 'when order by created_at descendingly' do
     let(:micropost_most_recent) { create(:micropost, user: user) }
     before(:each) do
       create_list(:micropost, 5, user: user, created_at: 3.hours.ago)
@@ -46,7 +46,7 @@ RSpec.describe Micropost, type: :model do
       create(:micropost, user: user, created_at: 2.hours.ago)
     end
 
-    it 'order should be most recent first' do
+    it 'should be most recent micropost first' do
       expect(Micropost.first).to eq micropost_most_recent
     end
   end
