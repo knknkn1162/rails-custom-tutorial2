8cf74799c1de9c925ebc66508cd1559e80cf48ea
knknkn1162 <knknkn1162@gmail.com>
2018-02-07 11:18:00 +0900

    refactor _header and _footer view spec

--- spec/views/layouts/_footer.html.erb_spec.rb
+++ spec/views/layouts/_footer.html.erb_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'layouts/_footer', type: :view do
-  it 'displays nav link' do
+  before(:each) do
     render
+  end
+
+  it 'displays nav link' do
     expect(rendered).to have_link 'About', href: '/about'
     expect(rendered).to have_link 'Contact', href: '/contact'
     expect(rendered).to have_link 'News'
